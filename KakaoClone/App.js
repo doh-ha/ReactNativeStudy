01babd764bd7bc5e1dd4095087111df57e6555e6
doh-ha <hayeonlr@gmail.com>
2023-02-02 19:03:01 +0900

    myProfile

--- KakaoClone/App.js
+++ KakaoClone/App.js
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import Header from "./Header";
+import Header from "./src/Header";
 import {
   getBottomSpace,
   getStatusBarHeight,
   ifIphoneX,
 } from "react-native-iphone-x-helper";
+import MyProfile from "./src/MyProfile";
+import { myProfile } from "./src/data";
+import Margin from "./src/Margin";
 
 const statusBarHeight = getStatusBarHeight(true);
 const bottomSpace = getBottomSpace();
@@ -15,6 +18,16 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style={styles.container} edges={["right", "left"]}>
         <Header />
+
+        {/* 사이 간격 */}
+        {/* <View style={{ height: 10 }} /> */}
+        <Margin height={10} />
+
+        <MyProfile
+          uri={myProfile.uri}
+          name={myProfile.name}
+          introduction={myProfile.introduction}
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
---
+++ KakaoClone/src/MyProfile.js
@@ -0,0 +1,26 @@
+import { View, Image, Text } from "react-native";
+import Margin from "./Margin";
+export default (props) => {
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <Image
+        source={{ uri: props.uri }}
+        style={{
+          width: 50,
+          height: 50,
+          borderRadius: 20,
+        }}
+      />
+      <Margin width={10} />
+      <View style={{ justifyContent: "center" }}>
+        <Text style={{ fontWeight: "bold", fontSize: 16 }}>{props.name}</Text>
+
+        <Margin height={6} />
+
+        <Text style={{ fontSize: 12, color: "grey" }}>
+          {props.introduction}
+        </Text>
+      </View>
+    </View>
+  );
+};
